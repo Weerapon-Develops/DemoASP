2a9cd2c7fb5b53f2e590782fa0abdb555a933464
Weeerapon.s <s.weerapon773@gmail.com>
2026-02-16 22:31:12 +0700

    Update : View and Model

--- DemoASP/Controllers/StudentController.cs
+++ DemoASP/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DemoASP.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DemoASP.Controllers
 {
@@ -6,7 +7,27 @@ namespace DemoASP.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            Student Student1 = new Student();
+            Student1.Id = 1;
+            Student1.Name = "Weerapon";
+            Student1.Score = 100;
+
+            var Student2 = new Student();
+            Student2.Id = 2;
+            Student2.Name = "SomChai";
+            Student2.Score = 50;
+
+            Student Student3 = new Student();
+            Student3.Id = 3;
+            Student3.Name = "SomMai";
+            Student3.Score = 42;
+
+            List<Student> allStudents =  new List<Student>();
+            allStudents.Add(Student1);
+            allStudents.Add(Student2);
+            allStudents.Add(Student3);
+
+            return View(allStudents);
         }
 
         public IActionResult Create()
--- DemoASP/Models/Student.cs
+++ DemoASP/Models/Student.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Score { get; set; }
+        public int Score { get; set; }
     }
 }
