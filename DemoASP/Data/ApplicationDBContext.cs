e140418244866e9d7b7a236d329babe445354930
Weeerapon.s <s.weerapon773@gmail.com>
2026-02-17 22:04:01 +0700

    Update : Entity Framwork Core

---
+++ DemoASP/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using DemoASP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoASP.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) :base(options) { 
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
